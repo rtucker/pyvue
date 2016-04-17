699a88150055a656768d2c4b36ae2b5703ef9322
Ryan Tucker <git@ryantucker.us>
2016-04-17 23:43:44 +0000

    More on-the-fly edits...

--- localads.py
+++ localads.py
@@ -11,8 +11,25 @@ LEFT_JUSTIFY = b'\x19'
 outfile = '../Prevue/local.ads'
 
 my_ads = [
-    [" ** Try the Delicious **", " ** CHEESE SELECTION! **", "", " Available now at your local", " Cheese Vendor"],
-    [" ", " ", " You are now breathing manually."],
+    [
+        " " + " ".join(["\x03%02d%02d\x0321" % (d, d) for d in range( 0, 12)]),
+        " " + " ".join(["\x03%02d%02d\x0321" % (d, d) for d in range(12, 24)]),
+        " " + " ".join(["\x03%02d%02d\x0321" % (d, d) for d in range(24, 36)]),
+        " " + " ".join(["\x03%02d%02d\x0321" % (d, d) for d in range(36, 48)]),
+        " " + " ".join(["\x03%02d%02d\x0321" % (d, d) for d in range(48, 60)]),
+    ],
+    [
+        " " + " ".join(["\x03%02d%02d\x0321" % (d, d) for d in range(60, 72)]),
+        " " + " ".join(["\x03%02d%02d\x0321" % (d, d) for d in range(72, 84)]),
+        " " + " ".join(["\x03%02d%02d\x0321" % (d, d) for d in range(84, 96)]),
+        " " + " ".join(["\x03%02d%02d\x0321" % (d, d) for d in range(96, 100)]),
+        " \x0356d r i n k m o r e b a w l s\x0321"
+    ],
+
+
+    [" ** \x0341Try the Delicious\x0321 **", " ** \x0357CHEESE SELECTION!\x0321 **", "", " Available now at your local", " Cheese Vendor"],
+    [" ", " ", " \x0325You are now breathing manually."],
+    [" ", " *B* *A* *W* *L* *S* ", " ", " How many can YOU take? ", " "],
 ]
 
 def render_ad(ad):
